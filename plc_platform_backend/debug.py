3653a62c63a9a6fb80c94cffb84b9fe7a652fb0e
filippodaniotti <febbrosto@gmail.com>
2025-01-21 15:44:28 +0100

    wip logging

--- plc_platform_backend/debug.py
+++ plc_platform_backend/debug.py
@@ -1,6 +1,16 @@
+import logging
+
 import uvicorn
 
 from plc_platform_backend.main import app
 
+logging.getLogger("watchfiles").setLevel(logging.WARNING)
+
 if __name__ == "__main__":
-    uvicorn.run("debug:app", host="0.0.0.0", port=8000, reload="true")
+    uvicorn.run(
+        "debug:app",
+        host="0.0.0.0",
+        port=8000,
+        reload="true",
+        reload_excludes=["logs/"],
+    )
--- plc_platform_backend/main.py
+++ plc_platform_backend/main.py
@@ -1,8 +1,15 @@
+import logging
+
 from fastapi import FastAPI
+
+from plc_platform_backend.configuration.logging_utils import setup_logging
 
 from .routers import modules
 
-app = FastAPI()
+logger = logging.getLogger(__name__)
 
+setup_logging()
+
+app = FastAPI()
 
 app.include_router(modules.router)
